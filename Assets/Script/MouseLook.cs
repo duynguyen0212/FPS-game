a9d3a0c40925dd97918cae8392aa5d988b029c0a
duynguyen0212 <ngocduy.nguyen0212@gmail.com>
2023-09-12 13:40:26 -0400

    player's movement and environment

--- Assets/Script/MouseLook.cs
+++ Assets/Script/MouseLook.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 200f;
 
     public Transform playerBody;
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -19,5 +20,9 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
